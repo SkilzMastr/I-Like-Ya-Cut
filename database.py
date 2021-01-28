c883592af70fc2f94dcd424911978b794cb35bbd
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2021-01-27 20:26:32 -0500

    It works, for now...

--- database.py
+++ database.py
@@ -1,31 +1,43 @@
-import pymysql.cursors
-
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='password',
-                             database='cut',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-with connection:
-
-    def checkUser(id):
-            with connection.cursor() as cursor:
-                sql = "SELECT `likes` FROM `users` WHERE `userID`=%s"
-                cursor.execute(sql, (id))
-                result = cursor.fetchone()
-                if result is None:
-                    return addUser(id)
-                else:
-                    return result
+import mysql.connector
 
 
-    def addUser(id):
-            with connection.cursor() as cursor:
-                sql = "INSERT INTO `users` (`userID`, `likes`) VALUES (%s, %s)"
-                cursor.execute(sql, (id, '1'))
-            connection.commit()
 
 
-print(checkUser('2'))
+
+
+def userInfo(id: int):
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="cut"
+    )
+    mycursor = db.cursor()
+    mycursor.execute(f"SELECT `likes` FROM `users` where `userID`={id}")
+    for x in mycursor:
+        return x
+
+def addUser(id: int):
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="cut"
+    )
+    mycursor = db.cursor()
+    mycursor.execute(f'INSERT INTO `users` (userID, likes) VALUES ({id}, 1);')
+    db.commit()
+    return True
+
+
+def addLike(id: int):
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password",
+        database="cut"
+    )
+    mycursor = db.cursor()
+    mycursor.execute(f"UPDATE `users` SET `likes` = `likes` + 1 WHERE `userID` = {id}")
+    db.commit()
+    return True
--- main.py
+++ main.py
@@ -18,7 +18,14 @@ async def likeCut(ctx, user: discord.Member):
     if ctx.author.id == user.id:
         await ctx.send(user.mention + ", You can't like your own cut!")
     else:
-        await ctx.send(database.checkUser(str(user.id)))
+        if database.userInfo(user.id) is None:
+            database.addUser(user.id)
+            await ctx.send(f"{ctx.author.mention} liked {user.mention}'s cut. They got their first like!")
+        else:
+            database.addLike(user.id)
+            likes = str(database.userInfo(user.id))
+            clean = likes.replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            await ctx.send(f"{ctx.author.mention} liked {user.mention}'s cut. They now have {clean} likes.")
 
 
 
